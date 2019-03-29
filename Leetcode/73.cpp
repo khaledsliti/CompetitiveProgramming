8ab867ceace6f570eae8dd694571a9c0378ac5ae
Khaled Sliti <khaled.sliti@supcom.tn>
2019-03-29 20:23:19 +0100

    Leetcode 341 388 394 406

--- Leetcode/73.cpp
+++ Leetcode/73.cpp
@@ -6,13 +6,16 @@ public:
   void setZeroes(vector<vector<int>>& matrix) {
     int n = (int)matrix.size();
     int m = (int)matrix[0].size();
-    for(int i = 0 ; i < n ; i++){
+    int last_row = 0;
+    for(int j = 0 ; j < m ; j++)
+      if(matrix[n - 1][j] == 0)
+        last_row = 1;
+    for(int i = 0 ; i < n - 1 ; i++){ 
       bool contains_zero = 0;
       for(int j = 0 ; j < m ; j++){
         if(matrix[i][j] == 0){
           contains_zero = 1;
-          for(int k = i - 1 ; k >= 0 ; k--)
-            matrix[k][j] = 0;
+          matrix[n - 1][j] = 0;
         }
       }
       if(contains_zero)
@@ -20,10 +23,13 @@ public:
           matrix[i][j] = 0;
     }
     for(int j = 0 ; j < m ; j++){
-      if(matrix[0][j] == 0)
-        for(int i = 1 ; i < n ; i++)
+      if(matrix[n - 1][j] == 0)
+        for(int i = 0 ; i < n - 1 ; i++)
           matrix[i][j] = 0;
     }
+    if(last_row)
+      for(int j = 0 ; j < m ; j++)
+        matrix[n - 1][j] = 0;
   }
 };
 
