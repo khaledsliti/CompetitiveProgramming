884f4d1f56e849854d226cf0ced896a04b207f52
Khaled Sliti <khaled.sliti@supcom.tn>
2019-05-12 15:27:27 +0100

    CODECHEF ADAPWNS

---
+++ Codechef/ADAPWNS.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+const int mod = 1e9 + 7;
+
+int main()
+{
+  int T;
+  cin >> T;
+  while(T--){
+    string s;
+    cin >> s;
+    vector<int> v;
+    int cnt = 0;
+    for(int i = 0 ; i < sz(s) ; i++)
+      if(s[i] == 'P')
+        v.push_back(cnt), cnt = 0;
+      else
+        cnt++;
+    int ans = 0;
+    for(int i = sz(v) - 1 ; i >= 0 ; i -= 2)
+      ans ^= v[i] % 3;
+    cout << (ans ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
