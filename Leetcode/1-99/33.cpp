6713fd86c34e4605cce0925d84a51aace154e2e8
Khaled Sliti <khaled.sliti@supcom.tn>
2020-02-21 02:05:23 +0100

    Leetcode problems

--- Leetcode/1-99/33.cpp
+++ Leetcode/1-99/33.cpp
@@ -1,27 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define D(x) cerr << #x << " = " << (x) << '\n'
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
 class Solution {
 public:
-  int search(vector<int>& nums, int target) {
-    int n = (int)nums.size();
-    int l = 0, r = n - 1, st = n - 1;
-    while(l <= r){
-      int mid = (l + r) / 2;
-      if(nums[mid] <= nums[n - 1]){
-        st = mid;
-        r = mid - 1;
-      }else{
-        l = mid + 1;
+  int search(vector<int>& arr, int target) {
+    if(int n = arr.size(); n > 0){
+      int first = 0, l = 1, r = n - 1;
+      while(l <= r){
+        int mid = (l + r) >> 1;
+        if(arr[mid] < arr[0]){
+          r = mid - 1;
+          first = mid;
+        }else{
+          l = mid + 1;
+        }
       }
-    }
-    l = st, r = st + n - 1;
-    while(l <= r){
-      int mid = (l + r) / 2;
-      if(nums[mid % n] == target)
-        return mid % n;
-      if(nums[mid % n] < target)
-        l = mid + 1;
-      else
-        r = mid - 1;
+      int last = first + n - 1;
+      while(first <= last){
+        int mid = (first + last) >> 1;
+        int idx = mid % n;
+        if(arr[idx] == target)
+          return idx;
+        if(arr[idx] > target)
+          last = mid - 1;
+        else
+          first = mid + 1;
+      }
     }
     return -1;
   }
 };
+
+int main()
+{
+  return 0;
+}
