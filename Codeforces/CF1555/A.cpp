001d24fd85c9067f7fdb5d1d581863a015da532d
Khaled <khaled.sliti1@gmail.com>
2021-08-01 14:02:51 +0200

    CF1555 A & E

---
+++ Codeforces/CF1555/A.cpp
@@ -0,0 +1,30 @@
+// RedStone
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define D(x) cerr << #x << " = " << (x) << '\n'
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+typedef long long ll;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T;
+  cin >> T;
+  while(T--) {
+    long long n;
+    cin >> n;
+    n = max(n, 6LL);
+    if(n & 1) n++;
+    long long ans = n * 5 / 2;
+    cout << ans << endl;
+  }
+  return 0;
+}
